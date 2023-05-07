30a26ab932bf25c0649e693022fea69853f51033
Olliver <olliverordell@hotmail.com>
2023-05-07 11:53:04 +0200

    Changed logging pattern

--- Eclipse/src/eclipse/Log.cpp
+++ Eclipse/src/eclipse/Log.cpp
@@ -8,7 +8,7 @@ namespace eclipse {
 	std::shared_ptr<spdlog::logger> Log::client_logger_;
 
 	void Log::init() {
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		spdlog::set_pattern("%^[%T:%e] [%=7n] %v%$");
 
 		core_logger_ = spdlog::stdout_color_mt("ECLIPSE");
 		core_logger_->set_level(spdlog::level::trace);
