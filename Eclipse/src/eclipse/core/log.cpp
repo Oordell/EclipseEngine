7c822bbd65309f2849241e09e8c7ea23e646c155
Olliver <olliverordell@hotmail.com>
2023-07-16 18:55:28 +0200

    Log messages are now additionally sent to Eclipse.log file

--- Eclipse/src/eclipse/core/log.cpp
+++ Eclipse/src/eclipse/core/log.cpp
@@ -1,7 +1,8 @@
 #include "ecpch.h"
 #include "log.h"
 
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace eclipse {
 
@@ -9,13 +10,23 @@ ref<spdlog::logger> Log::core_logger_;
 ref<spdlog::logger> Log::client_logger_;
 
 void Log::init() {
-	spdlog::set_pattern("%^[%T:%e] [%=7n] %v%$");
+	std::vector<spdlog::sink_ptr> log_sinks;
 
-	core_logger_ = spdlog::stdout_color_mt("ECLIPSE");
+	log_sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	log_sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/Eclipse.log", true));
+
+	log_sinks[0]->set_pattern("%^[%T:%e] [%=5!l] [%=7n] %v%$");
+	log_sinks[1]->set_pattern("[%T] [%=5!l] [%=7n]: %v");
+
+	core_logger_ = std::make_shared<spdlog::logger>("ECLIPSE", begin(log_sinks), end(log_sinks));
+	spdlog::register_logger(core_logger_);
 	core_logger_->set_level(spdlog::level::trace);
+	core_logger_->flush_on(spdlog::level::trace);
 
-	client_logger_ = spdlog::stdout_color_mt("APP");
+	client_logger_ = std::make_shared<spdlog::logger>("APP", begin(log_sinks), end(log_sinks));
+	spdlog::register_logger(client_logger_);
 	client_logger_->set_level(spdlog::level::trace);
+	client_logger_->flush_on(spdlog::level::trace);
 }
 
 }  // namespace eclipse
