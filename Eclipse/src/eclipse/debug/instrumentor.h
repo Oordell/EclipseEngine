8599f125203529868751121dfeb1edcad7414bff
Olliver <olliverordell@hotmail.com>
2023-07-16 17:53:44 +0200

    Auto deducing an availabe __FUNCSIG__ definition

--- Eclipse/src/eclipse/debug/instrumentor.h
+++ Eclipse/src/eclipse/debug/instrumentor.h
@@ -144,10 +144,30 @@ private:
 
 /* clang-format off */
 #if EC_PROFILE_ENABLED
+	// Resolve which function signature macro will be used. Note that this only is resolved when the (pre)compiler 
+	// starts, so the syntax highlighting could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define EC_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define EC_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define EC_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define EC_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define EC_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define EC_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define EC_FUNC_SIG __func__
+	#else
+		#define EC_FUNC_SIG "EC_FUNC_SIG unknown!"
+	#endif
+
 	#define EC_PROFILE_BEGIN_SESSION(name, filepath) ::eclipse::debug::Instrumentor::get().begin_session(name, filepath)
 	#define EC_PROFILE_END_SESSION()                 ::eclipse::debug::Instrumentor::get().end_session()
 	#define EC_PROFILE_SCOPE(name)                   ::eclipse::debug::InstrumentationTimer timer##__LINE__(name)
-	#define EC_PROFILE_FUNCTION()                    EC_PROFILE_SCOPE(__FUNCSIG__)
+	#define EC_PROFILE_FUNCTION()                    EC_PROFILE_SCOPE(EC_FUNC_SIG)
 #else
 	#define EC_PROFILE_BEGIN_SESSION(name, filepath)
 	#define EC_PROFILE_END_SESSION()
