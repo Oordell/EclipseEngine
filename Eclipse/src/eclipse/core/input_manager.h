5ea4ba2f5109a84d5969f4eb29f8dcf9f260e297
Olliver <olliverordell@hotmail.com>
2023-08-03 15:03:17 +0200

    Refactored input manager

--- Eclipse/src/eclipse/core/input_manager.h
+++ Eclipse/src/eclipse/core/input_manager.h
@@ -7,32 +7,11 @@ namespace eclipse {
 
 class ECLIPSE_API InputManager {
 public:
-	InputManager(const InputManager&)            = delete;
-	InputManager& operator=(const InputManager&) = delete;
-	virtual ~InputManager()                      = default;
-
-	inline static bool is_key_pressed(int keycode) { return instance_->is_key_pressed_impl(keycode); }
-
-	inline static bool is_mouse_button_pressed(int button) { return instance_->is_mouse_button_pressed_impl(button); }
-
-	inline static Point2D get_mouse_pose() { return instance_->get_mouse_pose_impl(); }
-
-	inline static float get_mouse_pose_x() { return instance_->get_mouse_pose_x_impl(); }
-
-	inline static float get_mouse_pose_y() { return instance_->get_mouse_pose_y_impl(); }
-
-	static scope<InputManager> create();
-
-protected:
-	InputManager()                                        = default;
-	virtual bool is_key_pressed_impl(int keycode)         = 0;
-	virtual bool is_mouse_button_pressed_impl(int button) = 0;
-	virtual Point2D get_mouse_pose_impl()                 = 0;
-	virtual float get_mouse_pose_x_impl()                 = 0;
-	virtual float get_mouse_pose_y_impl()                 = 0;
-
-private:
-	static scope<InputManager> instance_;
+	static bool is_key_pressed(int keycode);
+	static bool is_mouse_button_pressed(int button);
+	static Point2D get_mouse_pose();
+	static float get_mouse_pose_x();
+	static float get_mouse_pose_y();
 };
 
 }  // namespace eclipse
--- Eclipse/src/platform/windows/windows_input.cpp
+++ Eclipse/src/platform/windows/windows_input.cpp
@@ -1,24 +1,24 @@
 #include "ecpch.h"
-#include "windows_input.h"
+#include "eclipse/core/input_manager.h"
 #include "eclipse/core/application.h"
 
 #include <GLFW/glfw3.h>
 
 namespace eclipse {
 
-bool WindowsInput::is_key_pressed_impl(int keycode) {
+bool InputManager::is_key_pressed(int keycode) {
 	auto window = static_cast<GLFWwindow*>(Application::get().get_window().get_native_window());
 	auto state  = glfwGetKey(window, keycode);
 	return state == GLFW_PRESS || state == GLFW_REPEAT;
 }
 
-bool WindowsInput::is_mouse_button_pressed_impl(int button) {
+bool InputManager::is_mouse_button_pressed(int button) {
 	auto window = static_cast<GLFWwindow*>(Application::get().get_window().get_native_window());
 	auto state  = glfwGetMouseButton(window, button);
 	return state == GLFW_PRESS;
 }
 
-Point2D WindowsInput::get_mouse_pose_impl() {
+Point2D InputManager::get_mouse_pose() {
 	auto window = static_cast<GLFWwindow*>(Application::get().get_window().get_native_window());
 	double x_pose {};
 	double y_pose {};
@@ -27,8 +27,8 @@ Point2D WindowsInput::get_mouse_pose_impl() {
 	return {.x = static_cast<float>(x_pose), .y = static_cast<float>(y_pose)};
 }
 
-float WindowsInput::get_mouse_pose_x_impl() { return get_mouse_pose_impl().x; }
+float InputManager::get_mouse_pose_x() { return get_mouse_pose().x; }
 
-float WindowsInput::get_mouse_pose_y_impl() { return get_mouse_pose_impl().y; }
+float InputManager::get_mouse_pose_y() { return get_mouse_pose().y; }
 
 }  // namespace eclipse
