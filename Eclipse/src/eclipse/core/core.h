465e5584f0288391ab5da18109cfa17861739aa6
Olliver <olliverordell@hotmail.com>
2023-08-07 14:25:37 +0200

    Changed EC_BIND_EVENT_FN to use lambda instead of std::bind

--- Eclipse/src/eclipse/core/core.h
+++ Eclipse/src/eclipse/core/core.h
@@ -4,7 +4,6 @@
 
 #include <memory>
 
-
 /* clang-format off */
 // Platform detection using predefined macros
 #ifdef _WIN32
@@ -103,7 +102,8 @@ constexpr auto BIT(T x) {
 	return (1 << x);
 }
 
-#define EC_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define EC_BIND_EVENT_FN(fn) \
+	[this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace eclipse {
 
